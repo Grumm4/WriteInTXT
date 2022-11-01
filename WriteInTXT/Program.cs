9aff77fe20accaba3ceadc21fb20f4b25a5e3b1e
Grumm <kirill7777xp@mail.ru>
2022-11-01 09:42:02 +0500

    Добавьте файлы проекта.

---
+++ WriteInTXT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+//Путь до файла
+StreamWriter sw = new StreamWriter("C:\\Users\\Kirill\\Desktop\\Test.txt");
+
+try
+{
+    //Запись текста
+    for (int i = 0; i < 100; i++)
+    {
+        //здесь запишется в файл 100 чисел в диапазоне от 0 до 999
+        Random rnd = new Random();
+        sw.Write($"{rnd.Next(0, 999)},");
+    }
+}
+catch (Exception e) { Console.WriteLine("Exception: " + e.Message); }
+finally
+{
+    //Закрываем файл
+    sw.Close();
+}
